71a3f998f43a4e29c5c5de48a9bf561a7165e750
BlueFisher <blue_fisher@qq.com>
2015-07-03 16:40:51 +0800

    complete the static page

---
+++ OrderSystem/Scripts/orderApp/app.js
@@ -0,0 +1,11 @@
+﻿/* global angular */
+/* global $ */
+var app = angular.module('orderApp',[]);
+
+app.directive('ngStaticHeight',function(){
+	return function($scope,$elem,attr){
+		$elem.height(
+			$(window).height()-$('section.search').height()-$('section.search').height()
+		);
+	};
+});
